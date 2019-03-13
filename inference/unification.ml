42bacee1f6f5944ef1bdacb2c995004350ba1dc1
David Sevilla <sevilladavid83@gmail.com>
2019-03-13 01:38:05 -0400

    Modify function type case and clean up code
    Fixed a bug in the implementation of the mma algorithm
    and used cleaner variable names and orders

--- inference/unification.ml
+++ inference/unification.ml
@@ -3,27 +3,27 @@ open Ast
 type unif_result = UOk of Subs.subst | UError of texpr*texpr
 
 let mgu goals =
-  let rec mma goals sub =
-    match goals with
-    | (FuncType (s1, t1), FuncType (s2, t2)) :: g ->
-        mma ((s1, t1) :: (s2, t2) :: g) sub
-    | (IntType, IntType) :: g
-    | (BoolType, BoolType) :: g ->
-        mma goals sub
-    | (VarType s1, VarType s2) :: g when s1=s2 ->
-        mma goals sub
-    | (VarType str, other) :: g
-    | (other, VarType str) :: g ->
+  let rec mma sub = function
+    | (FuncType (s1, t1), FuncType (s2, t2)) :: goals ->
+        mma sub ((s1, s2) :: (t1, t2) :: goals)
+    | (IntType, IntType) :: goals
+    | (BoolType, BoolType) :: goals ->
+        mma sub goals
+    | (VarType str1, VarType str2) :: goals when str1=str2 ->
+        mma sub goals
+    | (VarType str, other) :: goals
+    | (other, VarType str) :: goals ->
         if SetStr.mem str (fv_of_type other)
         then UError (VarType str, other)
         else begin
+          (* TODO: Cannot simply extend here or can we because we can assume no
+           * vartype of the same str will appear in here? Do we need to
+           * apply_env? *)
           Subs.extend sub str other;
-          mma (List.map (fun (t1, t2) ->
-            (Subs.apply_to_texpr sub t1, Subs.apply_to_texpr sub t2)) goals) sub
+          mma sub (List.map (fun (t1, t2) ->
+            (Subs.apply_to_texpr sub t1, Subs.apply_to_texpr sub t2)) goals)
         end
-    | (s, t) :: goals ->
-        UError(s, t)
-    | [] ->
-        UOk sub
-  in mma goals (Subs.create ())
+    | (s, t) :: goals -> UError (s, t)
+    | [] -> UOk sub
+  in mma (Subs.create ()) goals
 
